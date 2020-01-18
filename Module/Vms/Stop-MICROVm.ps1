4dabee911c7cc21f930159ddf5ed8c0beb03f2d7
Stefan Hoffmann <stho32github@gmail.com>
2020-01-18 16:12:50 +0100

    Stop a microcloud, wherever it may be

---
+++ Module/Vms/Stop-MICROVm.ps1
@@ -0,0 +1,21 @@
+function Stop-MICROVM {
+    <#
+        .SYNOPSIS
+        Stops the Micro Cloud VM on whatever host it may be
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]$Node,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [PSObject]$VM
+    )
+    
+    process {
+        Write-Host "- stopping $($VM.VMName) on $Node ..."
+        Invoke-Command -ComputerName $Node -ArgumentList $VM.VMName -ScriptBlock {
+            Param($vmName)
+            Get-VM $vmName | Stop-VM 
+        }
+    }
+}

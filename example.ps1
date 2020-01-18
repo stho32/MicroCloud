64548f40f0683d14280aa4637df6bd8efcfcf68e
Stefan Hoffmann <stho32github@gmail.com>
2020-01-18 17:00:26 +0100

    add measurement of time

--- example.ps1
+++ example.ps1
@@ -9,6 +9,8 @@ Import-Module "C:\Projekte\MicroCloud\Module\MicroCloud.psm1"
 
 Clear
 
+$start = Get-Date
+
 Write-Output ""
 Write-Output "- Retrieve an overview of available ram ressources" 
 $stats = Get-MICRONodeStats
@@ -18,14 +20,12 @@ Write-Output "- Grab yourself an overview of all running vms on all nodes"
 Get-MICROVM | Format-Table
 
 Write-Output ""
-Write-Output "- Create a new virtual machine based on an image as long as we have ressources" 
+Write-Output "- Create a new virtual machines based on an image..." 
 
-$ramPerVmNeededGB = 4
-While ( (($stats.RamTotalGB)|Measure-Object -Maximum).Maximum -ge $ramPerVmNeededGB ) {
-    Add-MICROVM -BaseImage Basis-Windows-Server-2019-Image | Format-Table
-
-    $stats = Get-MICRONodeStats
-}
+$creationJobs = 1..18 | ForEach-Object {
+    Add-MICROVM -BaseImage Basis-Windows-Server-2019-Image
+} 
+$creationJobs | Wait-Job | Receive-Job | Format-Table
 
 Write-Output ""
 Write-Output "- Retrieve an overview of available ram ressources" 
@@ -38,3 +38,7 @@ Get-MICROVM | Stop-MICROVM
 Write-Output ""
 Write-Output "- Remove all vms that are stopped right now" 
 Clear-MICROHV | Format-Table
+
+$stop = Get-Date
+$durationInSeconds = ($stop-$start).TotalSeconds
+Write-Output "Total duration in seconds: $durationInSeconds"

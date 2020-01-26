fd69663af6b90f42ce077f48029002752cb9be8e
Stefan Hoffmann <stho32github@gmail.com>
2020-01-26 14:09:12 +0100

    load posh-ssh when loading the master module

--- Module/MicroCloud.psm1
+++ Module/MicroCloud.psm1
@@ -11,6 +11,10 @@ $global:MICROCLOUD_MicroNodes = "MASTER", "NODE01", "NODE02"
 $global:MICROCLOUD_ImageDirectory = "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
 $global:MICROCLOUD_ImageNodeDirectory = "C:\Projekte\Images"
 
+# This module is needed to access the router using SSH.
+[Reflection.Assembly]::LoadFile("C:\Program Files\WindowsPowerShell\Modules\Posh-SSH\2.2\Assembly\Renci.SshNet.dll")
+Import-Module Posh-SSH
+
 Push-Location $PSScriptRoot
 
 Get-ChildItem -Filter "*.ps1" -Recurse | 

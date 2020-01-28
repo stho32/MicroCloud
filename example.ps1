7a1d2d48002469dda6a9b36e3a78ceffd3ba0268
Stefan Hoffmann <stho32github@gmail.com>
2020-01-28 21:35:41 +0100

    modify example to work with intents instead of vms directly

--- example.ps1
+++ example.ps1
@@ -31,14 +31,17 @@ Write-Output "- Retrieve an overview of available ram ressources"
 $stats = Get-MICRONodeStats
 $stats | Format-Table
 
-Write-Output "- Grab yourself an overview of all running vms on all nodes" 
-Get-MICROVM | Format-Table
+# Accesses the "real" ressources
+#Write-Output "- Grab yourself an overview of all running vms on all nodes" 
+#Get-MICROVM | Format-Table
+
+Get-MicroVMIntent | Format-Table
 
 Write-Output ""
-Write-Output "- Create a new virtual machines based on an image..." 
+Write-Output "- Add intents to add 18 machines..." 
 
 1..18 | ForEach-Object {
-    Add-MICROVM -BaseImage Basis-Windows-Server-2019-Image
+    Add-MICROVMIntent -BaseImage "Basis-Windows-Server-2019-Image"
 } | Format-Table
 
 Write-Output ""
@@ -46,13 +49,16 @@ Write-Output "- Retrieve an overview of available ram ressources"
 Get-MICRONodeStats | Format-Table
 
 Write-Output ""
-Write-Output "- Stop all MicroCloud VMs" 
-Get-MICROVM | Stop-MICROVM 
+Write-Output "- Invoke a processing step to perform all necessary tasks on the nodes" 
 
-Write-Output ""
-Write-Output "- Remove all vms that are stopped right now" 
-Clear-MICROHV 
+Invoke-MICROProcessingStep
+
+# declare the intent to stop all micro cloud vms
+Get-MICROVmIntent | Set-MICROVmIntentStop
+
+Invoke-MICROProcessingStep
 
 $stop = Get-Date
 $durationInSeconds = ($stop-$start).TotalSeconds
 Write-Output "Total duration in seconds: $durationInSeconds"
+

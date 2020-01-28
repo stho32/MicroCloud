fa8f1bf3d2bc10500ca99c9735d03e0f310170d5
Stefan Hoffmann <stho32github@gmail.com>
2020-01-28 18:50:19 +0100

    remove all configuration from the source and put it into the database

--- Module/MicroCloud.psm1
+++ Module/MicroCloud.psm1
@@ -5,12 +5,6 @@
 #>
 $ErrorActionPreference = "Stop"
 
-$global:MICROCLOUD_VMNamesStartWith = "MicroVM-"
-$global:MICROCLOUD_MicroNodes = "MASTER", "NODE01", "NODE02"
-
-$global:MICROCLOUD_ImageDirectory = "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
-$global:MICROCLOUD_ImageNodeDirectory = "C:\Projekte\Images"
-
 $global:MICROCLOUD_SQLServer = ".\SQLEXPRESS"
 $global:MICROCLOUD_SQLUsername = "MicroCloudUser"
 $global:MICROCLOUD_SQLPassword = "123123"

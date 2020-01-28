6d722b22e0c483714a2aabf545fc9fc02c846983
Stefan Hoffmann <stho32github@gmail.com>
2020-01-28 21:30:50 +0100

    remove odd example queries

--- docs/example-database-entries.sql
+++ docs/example-database-entries.sql
@@ -7,12 +7,3 @@ SELECT 'VMNamesStartWith', 'MicroVM-', 'When creating VMs all their names start 
 SELECT 'TheMastersLocalImageDirectoryBeforeDistribution', 'C:\Users\Public\Documents\Hyper-V\Virtual hard disks', 'We create disk images in the default Hyper-V directory on the master. That is the source directory for distribution of images.' UNION ALL
 SELECT 'TheNodesLocalImageDirectory', 'C:\Projekte\Images', 'Every node, including the master, has this directory. Thats where we store the disk images that are the real base images for our VMS.'
 
-EXEC AddMicroVM 'Basis-Windows-Server-2019-Image'
-EXEC AddMicroVM 'Basis-Windows-Server-2019-Image'
-EXEC AddMicroVM 'Basis-Windows-Server-2019-Image'
-EXEC AddMicroVM 'Basis-Windows-Server-2019-Image'
-EXEC AddMicroVM 'Basis-Windows-Server-2019-Image'
-
-SELECT * FROM VirtualMachine
-
-SELECT * FROM MICRONodeStats

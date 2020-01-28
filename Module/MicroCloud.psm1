5393e1b2bb51736943e6c6d2f960d44c2ba8da6a
Stefan Hoffmann <stho32github@gmail.com>
2020-01-28 18:38:43 +0100

    add sql server configuration to module

--- Module/MicroCloud.psm1
+++ Module/MicroCloud.psm1
@@ -11,6 +11,11 @@ $global:MICROCLOUD_MicroNodes = "MASTER", "NODE01", "NODE02"
 $global:MICROCLOUD_ImageDirectory = "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
 $global:MICROCLOUD_ImageNodeDirectory = "C:\Projekte\Images"
 
+$global:MICROCLOUD_SQLServer = ".\SQLEXPRESS"
+$global:MICROCLOUD_SQLUsername = "MicroCloudUser"
+$global:MICROCLOUD_SQLPassword = "123123"
+$global:MICROCLOUD_SQLDatabase = "MicroCloud"
+
 # This module is needed to access the router using SSH.
 [Reflection.Assembly]::LoadFile("C:\Program Files\WindowsPowerShell\Modules\Posh-SSH\2.2\Assembly\Renci.SshNet.dll")
 Import-Module Posh-SSH

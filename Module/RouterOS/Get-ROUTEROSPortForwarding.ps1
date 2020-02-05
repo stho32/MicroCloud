de03ef8eb6679a4e4d00a1dd167f2d22d1567cf0
Stefan Hoffmann <stho32github@gmail.com>
2020-02-05 21:08:35 +0100

    reduce port forwardings by masqerade rule and port forwardings of the master - they are not interesting for our purpose

--- Module/RouterOS/Get-ROUTEROSPortForwarding.ps1
+++ Module/RouterOS/Get-ROUTEROSPortForwarding.ps1
@@ -8,7 +8,10 @@ function Get-ROUTEROSPortForwarding {
     )
     
     Process {
+        $masterNodeIP = Get-MICROConfigurationValue -Name MasterIP
         $natRules = Invoke-ROUTEROS -command "/ip firewall nat print terse"
-        Import-ROUTEROSPortForwarding -portForwardingsTerse $natRules
+        Import-ROUTEROSPortForwarding -portForwardingsTerse $natRules |
+            Where-Object chain -eq "dstnat" | # only dstnat = not the masquerading rule
+            Where-Object { -not ($_.ToAddress -eq $masterNodeIP) } # not the port forwardings of the master node
     }
 }

eca6531ded8ffe895b97b82ddb15df5dbedab1be
Stefan Hoffmann <stho32github@gmail.com>
2020-02-05 21:23:13 +0100

    add example,, how port forwardings can be added

---
+++ Port-Forwardings.ps1
@@ -0,0 +1,12 @@
+﻿Remove-Module MicroCloud*
+Import-Module "C:\Projekte\MicroCloud\Module\MicroCloud.psm1"
+
+$portForwardings = Get-ROUTEROSPortForwarding 
+$nextFreePort = Get-ROUTEROSNextFreeExternalPort -ExistingPortForwardings $portForwardings 
+
+Add-ROUTEROSPortForwarding -ExternalPort $nextFreePort -LocalTargetIP "192.168.88.194" -LocalTargetPort 3389
+
+#Remove-ROUTEROSPortForwarding -number 3
+
+
+

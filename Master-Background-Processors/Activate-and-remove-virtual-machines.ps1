03dd2e1407deb6af8d0ca41115f8d68efe90a38b
Stefan Hoffmann <stho32github@gmail.com>
2020-03-07 15:00:50 +0100

    combine all workers together so that updating becomes easier

--- Master-Background-Processors/Activate-and-remove-virtual-machines.ps1
+++ Master-Background-Processors/Activate-and-remove-virtual-machines.ps1
@@ -1,15 +1,2 @@
-<#
-    .SYNOPSIS
-    Invokes the processor that     
-
-#>
-Set-Location $PSScriptRoot
-
-Import-Module "..\Module\MicroCloud.psm1"
-. ..\config.ps1
-
-while ($true) {
-    Write-Host "[$(Get-Date)] checking for new todos..."
-    Invoke-MICROProcessingStep
-    Start-Sleep -Seconds 1
-}
+Write-Host "[$(Get-Date)] checking for new todos..."
+Invoke-MICROProcessingStep

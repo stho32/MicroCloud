c58415551d24e857bc55ecdca23489680ae721fd
Stefan Hoffmann <stho32github@gmail.com>
2020-03-07 15:23:25 +0100

    add passive update of vm state information, so users can use shutdown and we regognize it.

--- Master-Background-Processors/Start.ps1
+++ Master-Background-Processors/Start.ps1
@@ -3,11 +3,18 @@ Set-Location $PSScriptRoot
 
 Import-Module "..\Module\MicroCloud.psm1"
 . ..\config.ps1
-
+[int]$roundCounter = 0
 
 While ($true) {
     Start-Sleep -Seconds 1
     
     . .\Activate-and-remove-virtual-machines.ps1
     . .\Port-Forwardings.ps1
+
+    # every 5th round we update the state information
+    if ( $roundCounter % 5 ) {
+        Invoke-MICROUpdateVMStatusInformation
+    }
+
+    $roundCounter += 1
 }
